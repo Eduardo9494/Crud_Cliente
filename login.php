afb6e8afc53674b37584f8ec2bfe053ce165d488
Eduardo9494 <1433341@senati.pe>
2023-09-28 18:45:40 -0500

    incluimos el encabezado.php y creamos una clase container

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+include_once "encabezado.php";
+?>
+<div class="container">
+    <div class="row m-5 no-gutters shadow-lg">
+        <div class="col-md-6 d-none d-md-block">
+            <img src="logo_principal.png" class="img-fluid" style="min-height:100%;" />
+        </div>
+        <div class="col-md-6 bg-white p-5">
+            <h3 class="pb-3">Iniciar sesión</h3>
+            <div>
+                
+             </div>
+        </div>
+    </div>
+</div>
