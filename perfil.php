8d13797c00a988c3e855c0730dd46491a51ccc3c
Eduardo9494 <1433341@senati.pe>
2023-09-28 18:40:22 -0500

    creamos una clase container

--- perfil.php
+++ perfil.php
@@ -9,3 +9,18 @@ $nombreUsuario = $_SESSION['usuario'];
 $idUsuario = $_SESSION['idUsuario'];
 ?>
 
+<div class="container">
+	<div class="alert alert-primary text-center shadow-sm rounded" role="alert">
+		<h1>
+		<h1><?= $nombreUsuario?></h1>
+		</h1>
+	</div>
+	
+	<?php include_once "cartas_totales.php"?>
+	<div class="text-center mt-3">
+		<a href="cambiar_password.php" class="btn btn-lg btn-primary">
+			<i class="fa fa-key"></i>
+			Cambiar contraseña
+		</a>
+	</div>
+</div>
