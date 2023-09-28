8d49d6ca9b4392d85547e1f707b65acf99834ee6
Eduardo9494 <1433341@senati.pe>
2023-09-28 18:41:40 -0500

    creamos y codificamos nuestro archivo navbar.php y hacemos el inicio

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-2 shadow   rounded">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" alt="" width="150" height="40" class="d-inline-block align-text-top">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active "  href="index.php">
+            <i class="fa fa-home"></i>
+            Inicio
+            </a>
+        </li>
+        
+        
+        </ul>
+    </div>
+  </div>
+</nav>
